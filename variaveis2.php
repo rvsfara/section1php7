2b5cd4374468b76c82eeed7744b4f7852e6bba4a
rvsfara <rvsfara@gmail.com>
2022-10-31 18:58:17 -0300

    Commite pelo kate variáveis 2
    variáveis 2

---
+++ variaveis2.php
@@ -0,0 +1,12 @@
+<?php
+    //url mais a interrogação
+    //?a=123
+    //http://localhost/rvs/section1/variaveis2.php?a=123
+    $nome = $_GET["a"];
+    var_dump($nome);
+    echo "<br/>";
+    $vlrConvertido = (int)$nome;
+    var_dump($vlrConvertido);
+    //duas variáveis na url
+    //http://localhost/rvs/section1/variaveis2.php?a=123&b=456
+?>
