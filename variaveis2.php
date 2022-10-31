cc776580f1df345d64ee1ff54a1a60a676308b84
rvsfara <rvsfara@gmail.com>
2022-10-31 19:00:18 -0300

    variáveis 2 em PHP

--- variaveis2.php
+++ variaveis2.php
@@ -9,4 +9,7 @@
     var_dump($vlrConvertido);
     //duas variáveis na url
     //http://localhost/rvs/section1/variaveis2.php?a=123&b=456
+    //pegar ip do usuário
+    $ip = $_SERVER["REMOTE_ADDR"];
+    echo $ip;
 ?>
