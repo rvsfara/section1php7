9b06b3b4750be673256361753086f35ed89fa386
rvsfara <rvsfara@gmail.com>
2022-11-01 16:24:45 -0300

    Manipulando Strings

---
+++ variaveis5.php
@@ -0,0 +1,33 @@
+<?php
+    $nome = "Rafael";
+    $nome2 = 'Vieira';
+    var_dump($nome, $nome2);
+    echo "<br/>";
+    echo "ABC $nome";
+    echo "<br/>";
+    echo "ABC $nome2";
+    echo "<br/>";
+    $nome = strtoupper($nome);//maiuscula
+    $nome2 = strtolower($nome2);//minuscula
+    echo $nome . " " . $nome2;
+    echo "<br/>";
+    $nomeSobrenome = $nome . " " . $nome2;
+    $nomeSobrenome = strtolower($nomeSobrenome);
+    echo ucfirst($nomeSobrenome);//a primeira palavra da String toda
+    echo "<br/>";
+    echo ucwords($nomeSobrenome);//a primeira de cada palavra
+    echo "<br/>";
+    $senhaComum = "Senha";
+    $senhaComum = str_replace("a", "4", $senhaComum);//Troca uma caracter por outro
+    $senhaComum = str_replace("e", "3", $senhaComum);
+    echo $senhaComum;
+    echo "<br/>";
+    $frase = "A repetição é mãe da retenção.";
+    $palavra = "mãe";
+    $q = strpos($frase, $palavra);
+    $texto = substr($frase, 0, $q);
+    var_dump($texto);
+    $texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
+    echo "<br/>";
+    var_dump($texto2);
+?>
