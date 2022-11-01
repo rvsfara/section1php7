6b3ce2da4431f9d99a5d29884f46d38352937c20
rvsfara <rvsfara@gmail.com>
2022-11-01 15:23:29 -0300

    funções em PHP

--- variaveis3.php
+++ variaveis3.php
@@ -1,14 +1,39 @@
 <?php
+    echo "Funções";
+    echo "<br/>";
     $nome = "Rafael";
     function teste(){
         global $nome;
         echo $nome . " teste1";
     }
     function teste2(){
-        echo "<br/>";
         $nome = "João";
         echo $nome . " agora no teste2";
     }
     teste();
+    echo "<br/>";
     teste2();
+    echo "<br/>";
+    echo "Operadores em PHP";
+    echo "<br/>";
+    $valorTotal = 0;
+    $valorTotal += 100;
+    $valorTotal += 25;
+    $valorTotal += -10;
+    $valorTotal *= .9;
+    echo $valorTotal;
+    $a = 10;
+    $b = 2;
+    echo $a + $b;
+    echo "<br/>";
+    echo $a - $b;
+    echo "<br/>";
+    echo $a * $b;
+    echo "<br/>";
+    echo $a / $b;
+    echo "<br/>";
+    echo $a % $b;//sobra 0
+    echo "<br/>";
+    echo $a ** $b;//exponenciação 10 elevado ao quadrado
+
 ?>
